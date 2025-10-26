57143edf1e9ad677750fbf92c5f027f80e692284
AbdulghaniAlhamwi <AbdulghaniAlhamwi3@gmail.com>
2025-10-26 16:36:01 +0300

    Version 1

---
+++ Simple Islamic Project/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Simple_Islamic_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblContainer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblContainer
+            // 
+            this.lblContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblContainer.Location = new System.Drawing.Point(38, 12);
+            this.lblContainer.Name = "lblContainer";
+            this.lblContainer.Size = new System.Drawing.Size(357, 140);
+            this.lblContainer.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.btnStart.Location = new System.Drawing.Point(171, 105);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnStart.Size = new System.Drawing.Size(87, 34);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "ابدأ";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtContent);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.lblContainer);
+            this.panel1.Location = new System.Drawing.Point(188, 163);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 183);
+            this.panel1.TabIndex = 3;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // txtContent
+            // 
+            this.txtContent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtContent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContent.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.txtContent.Location = new System.Drawing.Point(49, 24);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ReadOnly = true;
+            this.txtContent.Size = new System.Drawing.Size(330, 65);
+            this.txtContent.TabIndex = 2;
+            this.txtContent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(211)))), ((int)(((byte)(112)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Islamic Project";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblContainer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ Simple Islamic Project/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Islamic_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       private enum _entypes
+        {
+          سبحان_الله =1,
+            الحمدلله = 2,
+           الله_أكبر=3,
+            لا_إله_إلا_الله=4
+        }
+
+        _entypes _Types= _entypes.سبحان_الله;
+
+        short _Counter = 0;
+        bool _ChangeColor = false;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            timer1.Start();
+            txtContent.ForeColor = Color.FromArgb(255,250,250,250);
+
+        }
+
+        void _ChangeContentColor()
+        {
+            if(_ChangeColor)
+            switch(_Types)
+                { 
+
+                case _entypes.الحمدلله:
+                        txtContent.ForeColor = Color.OrangeRed;
+                        break;
+
+                case _entypes.الله_أكبر:
+                        txtContent.ForeColor = Color.Gold;
+                        break;
+
+                    case _entypes.لا_إله_إلا_الله:
+                        txtContent.ForeColor = Color.GreenYellow;
+                        break;
+                }
+            _ChangeColor = false;
+            }
+        void _ContentModifier(string content)
+        {
+            txtContent.Text = content;
+        }
+
+        void _SetNotifyIcon(string MessageBode,string Title)
+        {
+            notifyIcon1.Text = "Message";
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = MessageBode;
+            notifyIcon1.BalloonTipTitle = Title;
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+        void _EndProgram()
+        {
+            btnStart.Enabled = true;
+            timer1.Stop();
+            _SetNotifyIcon("في ميزان حسناتكم", "Notification");
+              _Types = _entypes.سبحان_الله;
+            _Counter = 0;
+        }
+
+        void _SetProgramProperties()
+        {
+            if (_Counter==33 && _Types!=_entypes.لا_إله_إلا_الله)
+            {
+                _Counter = 0;
+                switch (_Types)
+                {
+                    case _entypes.سبحان_الله:
+                        _Types = _entypes.الحمدلله;
+                        _ChangeColor = true;
+                        break;
+
+                    case _entypes.الحمدلله:
+                        _Types = _entypes.الله_أكبر;
+                        _ChangeColor = true;
+                        break;
+
+                    case _entypes.الله_أكبر:
+                        _Types = _entypes.لا_إله_إلا_الله;
+                        _ChangeColor = true;
+                        break;
+                }
+            }
+            else if (_Counter == 33&&_Types == _entypes.لا_إله_إلا_الله)
+            {
+                _EndProgram();
+            }
+        }
+
+        void _GetVisualContent()
+        {
+            switch( _Types)
+            {
+                case _entypes.سبحان_الله:
+            _ContentModifier(_Counter + " _  سبحان الله");
+                    break;
+
+                case _entypes.الحمدلله:
+                    _ContentModifier(_Counter + " _  الحمد الله");
+                    break;
+
+                case _entypes.الله_أكبر:
+                    _ContentModifier(_Counter + " _  الله أكبر");
+                    break;
+
+                case _entypes.لا_إله_إلا_الله:
+                    _ContentModifier("لا إله إلا الله وحده لا شريك له  له الملك و له الحمد و هو على كل شيء قدير");
+                    break;
+
+            }
+            _ChangeContentColor();
+            _SetProgramProperties();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _Counter++;
+            _GetVisualContent();
+        }
+    }
+}
